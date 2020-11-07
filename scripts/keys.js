c6b01d2bed23aa675b0a8721979e5e363d5cd476
Walter Reis <xinchro@xinchronize.com>
2020-11-07 23:15:07 +0000

    added keyboard events

--- scripts/keys.js
+++ scripts/keys.js
@@ -3,68 +3,91 @@ console.log("Starting keys")
 let keyNames = [
   {
     'text': '1',
+    'code': 'Digit1',
     'callback': function () { tempKeyPressed('1') }
   },
   {
     'text': '2',
+    'code': 'Digit2',
     'callback': function () { tempKeyPressed('2') }
   },
   {
     'text': '3',
+    'code': 'Digit3',
     'callback': function () { tempKeyPressed('3') }
   },
   {
     'text': 'q',
+    'code': 'KeyQ',
     'callback': function () { tempKeyPressed('q') }
   },
   {
     'text': 'w',
+    'code': 'KeyW',
     'callback': function () { tempKeyPressed('w') }
   },
   {
     'text': 'e',
+    'code': 'KeyE',
     'callback': function () { tempKeyPressed('e') }
   },
   {
     'text': 'a',
+    'code': 'KeyA',
     'callback': function () { tempKeyPressed('a') }
   },
   {
     'text': 's',
+    'code': 'KeyS',
     'callback': function () { tempKeyPressed('s') }
   },
   {
     'text': 'd',
+    'code': 'KeyD',
     'callback': function () { tempKeyPressed('d') }
   },
   {
     'text': 'z',
+    'code': 'KeyZ',
     'callback': function () { tempKeyPressed('z') }
   },
   {
     'text': 'x',
+    'code': 'KeyX',
     'callback': function () { tempKeyPressed('x') }
   },
   {
     'text': 'c',
+    'code': 'KeyC',
     'callback': function () { tempKeyPressed('c') }
   },
   {
-    'text': 'ctrl',
+    'text': 'control',
+    'code': 'ControlLeft',
     'callback': function () { tempKeyPressed('ctrl') }
   },
   {
     'text': 'shift',
+    'code': 'ShiftLeft',
     'callback': function () { tempKeyPressed('shift') }
   },
   {
     'text': 'space',
+    'code': 'Space',
     'callback': function () { tempKeyPressed('space') }
   },
 ]
 
 function tempKeyPressed(text) {
   addMessage(`${text} pressed!`)
+}
+
+document.addEventListener('keyup', keyboardHandler)
+function keyboardHandler(event) {
+  let keyPressed = keyNames.find((key) => {
+    return event.code === key.code
+  })
+  keyPressed && keyPressed.callback()
 }
 
 function createKeyGridDOM(){
